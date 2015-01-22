4737957e71ddb26ee2ce7b31c7ff023b0d0d7a63
user <user@localhost.localdomain>
2015-01-22 16:38:33 +0900

    changed message again

--- hangman.py
+++ hangman.py
@@ -59,7 +59,12 @@ HANGMANPICS = ['''
  / \  |
       |
 =========''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+
+def readWordList():
+    file = open('text.txt','r')
+    return file.read().split()
+
+words = readWordList()
 
 def getRandomWord(wordList):
     # This function returns a random string from the passed list of strings.
